344688dc4ff387dbf181ac85497f5f08ac97351e
DahKoul <andriamanampisoakoloina@gmail.com>
2026-05-12 11:21:17 +0300

    e

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,15 +3,11 @@
 use CodeIgniter\Router\RouteCollection;
 
 /**
- * @var RouteCollection $routes
- */
+ 
+@var RouteCollection $routes*/
 $routes->setDefaultController('IndexController');
 $routes->setDefaultMethod('index');
 
-$routes->get('/', 'AuthController::login');
-
-$routes->get('/login',     'AuthController::login');
-$routes->post('/login',    'AuthController::loginPost');
-$routes->get('/register',  'AuthController::register');
-$routes->post('/register', 'AuthController::registerPost');
-$routes->get('/logout',    'AuthController::logout');
+$routes->get('/', 'IndexController::index');
+$routes->get('/index', 'IndexController::index');
+$routes->get('/creneaux', 'CreneauxController::index');
